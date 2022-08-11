fae6b5d1c22221e3853c36b6b5fc7a546aa0a2d6
AndrewNagikh <nagikhandrew@gmail.com>
2022-08-11 12:36:32 +0300

    add express-session config

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 // const { sequelize } = require('../db/models');
 const renderTemplate = require('../lib/renderReactModule');
@@ -16,7 +18,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Выносим порт в .env и на всякий случай подставляем дефолтный через ||
-const PORT = process.env.PORT || 3001;
+const { PORT, SESSION_SECRET } = process.env;
+
+const sessionConfig = {
+  name: 'your coockie name', // * Название куки
+  store: new FileStore(), // * подключение стора (БД для куки) для хранения
+  secret: SESSION_SECRET ?? 'your key', // * ключ для шифрования куки
+  resave: false, // * если true, пересохраняет сессию, даже если она не поменялась
+  saveUninitialized: false, // * Если false, куки появляются только при установке req.session
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 10, // * время жизни в ms (10 дней)
+    httpOnly: true, // * куки только по http
+  },
+};
+
+app.use(session(sessionConfig));
 
 app.listen(PORT, async () => {
   try {
