6644a6e14977e87b662c1b0d3198580786e97ecf
Ruslan <rrrruuus@yandex.ru>
2022-09-06 14:30:58 +0300

    db

---
+++ db/migrations/20220906105214-create-album.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Albums', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userid: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Albums');
+  }
+};
---
+++ db/models/photo.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Photo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.Album, { foreignKey: 'albumid' });
+      // define association here
+    }
+  }
+  Photo.init({
+    albumid: DataTypes.INTEGER,
+    comment: DataTypes.STRING,
+    addres: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Photo',
+  });
+  return Photo;
+};
