000d455be13d4d70fe149531cafedb71f9d9e3fe
Andrew <yau7237@gmail.com>
2022-09-06 16:39:56 +0500

    some views and auth/reg

---
+++ views/RegisterForm.jsx
@@ -0,0 +1,20 @@
+const React = require('react');
+const Layout = require('./Layout');
+
+function SignUpForm() {
+  return (
+    <Layout title="Form">
+      <h1>Sign UP</h1>
+      <form action="/signup" method="POST" >
+        <input type="text" name="userName" placeholder="userName" />
+        <input type="text" name="name" placeholder="name" />
+        <input type="text" name="surname" placeholder="surname" />
+        <input type="password" name="password" placeholder="password" />
+        <input type="email" name="email" placeholder="email" />
+        <button type="submit">Register!</button>
+      </form>
+    </Layout>
+  );
+}
+
+module.exports = SignUpForm;
